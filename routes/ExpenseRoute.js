86e302145f45f5090bdfdca8e1ac3294d8a74b45
dimdejesus <ddejesus0320@gmail.com>
2022-05-18 14:59:30 +0800

    Setting up the Expense Route with its Controller

--- routes/ExpenseRoute.js
+++ routes/ExpenseRoute.js
@@ -2,21 +2,30 @@
 
 import express from "express";
 
-//Controllers
-import { findAll } from "../controller/ExpenseController.js";
+//Controllers where the logic comes from
+import {
+	findAll,
+	findById,
+	createExpense,
+	updateExpense,
+	expensePerWeek,
+	deleteById,
+} from "../controller/ExpenseController.js";
 
 //Initialize Express Router
 const router = express.Router();
 
 //Get All Expenses
 router.get("/expenses", findAll);
+//Get Total Expenses in a Week
+router.get("/expenses/week", expensePerWeek);
 //Get One Expense
-router.get("/expenses/:id");
+router.get("/expenses/:id", findById);
 //Create New Expense
-router.post("/expenses");
+router.post("/expenses", createExpense);
 //Update One Expense
-router.put("/expenses/:id");
+router.put("/expenses/:id", updateExpense);
 //Delete One Expense
-router.delete("/expenses/:id");
+router.delete("/expenses/:id", deleteById);
 
 export default router;
